d8417e42109e3644734426df1893515a468b6fe3
zhangtianxin <1582158644@qq.com>
2020-08-16 23:47:01 +0800

    文件上传相关

---
+++ src/main/java/com/example/demomb/common/FileUtil.java
@@ -0,0 +1,95 @@
+package com.example.demomb.common;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileUtil {
+    /**
+     * file转byte
+     */
+    public static byte[] file2byte(File file){
+        byte[] buffer = null;
+        try{
+            FileInputStream fis = new FileInputStream(file);
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            byte[] b = new byte[1024];
+            int n;
+            while ((n = fis.read(b)) != -1)
+            {
+                bos.write(b, 0, n);
+            }
+            fis.close();
+            bos.close();
+            buffer = bos.toByteArray();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return buffer;
+    }
+
+    /**
+     * byte 转file
+     */
+    public static File byte2File(byte[] buf, String filePath, String fileName){
+        BufferedOutputStream bos = null;
+        FileOutputStream fos = null;
+        File file = null;
+        try{
+            File dir = new File(filePath);
+            if (!dir.exists() && dir.isDirectory()){
+                dir.mkdirs();
+            }
+            file = new File(filePath + File.separator + fileName);
+            fos = new FileOutputStream(file);
+            bos = new BufferedOutputStream(fos);
+            bos.write(buf);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            if (bos != null){
+                try{
+                    bos.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            if (fos != null){
+                try{
+                    fos.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return file;
+    }
+    /**
+     * multipartFile转File
+     **/
+    public static File multipartFile2File(MultipartFile multipartFile){
+        File file = null;
+        if (multipartFile != null){
+            try {
+                file=File.createTempFile("tmp", null);
+                multipartFile.transferTo(file);
+                System.gc();
+                file.deleteOnExit();
+            }catch (Exception e){
+                e.printStackTrace();
+                // log.warn("multipartFile转File发生异常："+e);
+            }
+        }
+        return file;
+    }
+}
--- src/main/java/com/example/demomb/controller/TestController.java
+++ src/main/java/com/example/demomb/controller/TestController.java
@@ -1,20 +1,33 @@
 package com.example.demomb.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.example.demomb.common.FileUtil;
 import com.example.demomb.controller.model.ResponseCode;
 import com.example.demomb.controller.model.ResponseModel;
 import com.example.demomb.service.internal.CompanyServiceImpl;
 import com.example.demomb.service.internal.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Date;
 
 @Api(value = "test controller")
@@ -46,6 +59,33 @@ public class TestController extends BaseController {
             String fileName = jsonObject.getString("fileName");
             if (StringUtils.isNotBlank(fileName)) {
                 byte[] filContents = jsonObject.getBytes("filContent");
+                File test = FileUtil.byte2File(filContents, "test", fileName);
+
+                InputStream inputStream = new ByteArrayInputStream(filContents);
+                // MultipartFile file = new MockMultipartFile("新文件名","原文件名",ContentType.APPLICATION_OCTET_STREAM.toString(), inputStream);
+                MultipartFile file = new MockMultipartFile("新文件名", "原文件名", "text/plain", inputStream);
+                //有个问题，https://www.zhihu.com/question/53215827?sort=created
+                //    问题是这个spring-test依赖只用于测试，项目打包后就不存在了，除非是没有maven，gradle的自由式打包
+
+                //    https://www.cnblogs.com/youyouxiaosheng-lh/p/11026763.html,需要试验一下
+                FileItemFactory factory = new DiskFileItemFactory(16, null);
+                String textFieldName = "textField";
+                FileItem item = factory.createItem(textFieldName, "text/plain", true, fileName);
+                int bytesRead = 0;
+                byte[] buffer = new byte[8192];
+                try {
+                    FileInputStream fis = new FileInputStream("savePath" + fileName);
+                    OutputStream os = item.getOutputStream();
+                    while ((bytesRead = fis.read(buffer, 0, 8192)) != -1) {
+                        os.write(buffer, 0, bytesRead);
+                    }
+                    os.close();
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                MultipartFile multipartFile = new CommonsMultipartFile(item);
+                //    https://bbs.csdn.net/topics/320256753 需要额外导入两个包
 
             }
             companyService.create(jsonObject);
